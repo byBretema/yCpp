632972cdfbcea22a524f19129712d72f1ae10136
ByBretema <bybretema@gmail.com>
2025-10-01 18:32:48 +0200

    improving cmake layer

--- run.py
+++ run.py
@@ -4,59 +4,107 @@ import shutil
 import subprocess
 import argparse
 import mimetypes
+from pathlib import Path
+
+################################################################################
+# Globals
 
 BUILD_DIR = "build"
 TESTS_DIR = "tests"
 
 
+################################################################################
+def print_deco(
+    title: str | None,
+    pre_ln: bool = False,
+    post_ln: bool = False,
+    align_right: bool = False,
+    sep_offset: int = 0,  # len() could miss-count when the string contains emojis
+):
+
+    template = "-- ❱{t}{s}❰"
+    if title:
+        template = "-- ❱ {t} ❰{s}❰"
+        if align_right:
+            template = "-- ❱{s}❱ {t} ❰"
+    else:
+        title = ""
+
+    template_with_title = template.format(t=title, s="")
+    n_sep = max(80 - len(template_with_title) - sep_offset, 0)
+    sep = "-" * n_sep
+
+    if pre_ln:
+        print("")
+
+    print(template.format(t=title, s=sep))
+
+    if post_ln:
+        print("")
+
+
+################################################################################
 def command_exists(cmd: str) -> bool:
     return shutil.which(cmd) is not None
 
 
+################################################################################
 def error_exit(message: str) -> None:
     print(f"[ERR] - {message}", file=sys.stderr)
     sys.exit(1)
 
 
-def is_binary_by_mimetype(filepath):
+################################################################################
+def is_binary_file(file_path, chunk_size=1024, null_byte_threshold=0.1):
+    if not os.path.exists(file_path):
+        return False
+
     try:
-        mime_type, _ = mimetypes.guess_type(filepath)
-        if mime_type:
-            return not mime_type.startswith('text/')
-    except:
-        pass
-    return False
+        with open(file_path, "rb") as f:
+            chunk = f.read(chunk_size)
+    except IOError:
+        return False
+
+    null_count = chunk.count(b"\x00")
+    if null_count / len(chunk) > null_byte_threshold:
+        return True
 
 
+################################################################################
 def main():
+
+    ############################################################################
+    # Args
+
     parser = argparse.ArgumentParser(description="Build script")
-    parser.add_argument(
-        "-c", "--cleanup", action="store_true", help="Cleanup sub-build directory"
-    )
-    parser.add_argument(
-        "-C", "--fullcleanup", action="store_true", help="Cleanup build directory"
-    )
-    parser.add_argument(
-        "-r", "--release", action="store_true", help="Build in Release mode"
-    )
-    parser.add_argument(
-        "-g", "--cmakegen", type=str, default="Ninja", help="CMake generator"
-    )
+
+    parser.add_argument("-t", "--tests", action="store_true", help="Run project tests")
+    parser.add_argument("-c", "--cleanup", action="store_true", help="Cleanup sub-build directory")
+    parser.add_argument("-C", "--fullcleanup", action="store_true", help="Cleanup build directory")
+    parser.add_argument("-r", "--release", action="store_true", help="Build in Release mode")
+    parser.add_argument("-g", "--cmakegen", type=str, default="Ninja", help="CMake generator")
+
     args = parser.parse_args()
 
-    # Check required commands
-    if not command_exists("cmake"):
-        error_exit("Command 'cmake' is required")
+    ############################################################################
+    # Variables
 
     build_type = "Release" if args.release else "Debug"
+    sub_build_dir = f"{BUILD_DIR}/project"
     cmake_gen = args.cmakegen
+
+    ############################################################################
+    # Check required commands
+
+    if not command_exists("cmake"):
+        error_exit("Command 'cmake' is required")
 
     if cmake_gen == "Ninja" and not command_exists("ninja"):
         error_exit("Command 'ninja' is required")
 
-    sub_build_dir = f"{BUILD_DIR}/project"
-
+    ############################################################################
     # Cleanup
+
     if args.fullcleanup and os.path.isdir(BUILD_DIR):
         shutil.rmtree(BUILD_DIR)
     os.makedirs(BUILD_DIR, exist_ok=True)
@@ -65,39 +113,59 @@ def main():
         shutil.rmtree(sub_build_dir)
     os.makedirs(sub_build_dir, exist_ok=True)
 
-    # Change to build directory
-    os.chdir(sub_build_dir)
+    ############################################################################
+    # Config + Build
 
-    # Config
+    print(f"\n\n# CONFIG\n")
+    print_deco(None)
     config_cmd = ["cmake", "-G", cmake_gen, "../..", f"-DCMAKE_BUILD_TYPE={build_type}"]
-    subprocess.run(config_cmd, check=True)
+    subprocess.run(config_cmd, check=True, cwd=sub_build_dir)
+    print_deco(None)
 
-    # Build
-    build_cmd = [
-        "cmake",
-        "--build",
-        ".",
-        "-j",
-        str(os.cpu_count()),
-        "--config",
-        build_type,
-    ]
-    subprocess.run(build_cmd, check=True)
+    print(f"\n\n# BUILD\n")
+    build_cmd = ["cmake", "--build", ".", "-j", str(os.cpu_count()), "--config", build_type]
+    subprocess.run(build_cmd, check=True, cwd=sub_build_dir)
 
+    ############################################################################
     # Tests
-    tests_dir = f"{BUILD_DIR}/tests"
 
-    if os.path.isdir(tests_dir):
-        for root, _, files in os.walk(tests_dir):
-            for filename in files:
-                filepath = os.path.join(root, filename)
-                if is_binary_by_mimetype(filepath):
-                    print(f"-- Running TEST : {filename}")
+    if args.tests:
+        print(f"\n\n# TESTs")
+        tests_dir = os.path.abspath(f"{BUILD_DIR}/tests")
 
-    # Cleanup
-    if os.path.isdir(tests_dir):
+        tests_total = 0
+        tests_passed = 0
+
+        if os.path.isdir(tests_dir):
+
+            for root, _, files in os.walk(tests_dir):
+                for filename in files:
+                    filepath = os.path.join(root, filename)
+
+                    if is_binary_file(filepath):
+                        tests_total += 1
+
+                        print_deco(filename, pre_ln=True)
+
+                        p = subprocess.run([filepath], capture_output=True)
+                        ok = not p.returncode
+
+                        tests_passed += int(ok)
+
+                        mark = "🟢" if ok else "🔴"
+                        status = "PASS" if ok else "FAIL"
+                        print_deco(f"{status}  {mark} ", align_right=True, sep_offset=1)
+
+        ok = tests_passed == tests_total
+
+        mark = "✅️" if ok else "❌️"
+        status = "PASS" if ok else "FAIL"
+        print(f"\n-- Passed Tests ({tests_passed}/{tests_total}) ❱ {status} {mark}")
+
+    ############################################################################
 
 
+################################################################################
 if __name__ == "__main__":
     try:
         main()
--- vendor/y.cmake
+++ vendor/y.cmake
@@ -31,7 +31,7 @@
 include(FetchContent)
 
 
-###############################################################################
+################################################################################
 ## Vars !
 
 # Output Folder
@@ -40,8 +40,10 @@ set(FETCHCONTENT_BASE_DIR ${CMAKE_SOURCE_DIR}/build/deps)
 # Global Variable w/ lib names
 set(y_meta_libs "" CACHE INTERNAL "")
 
+set(__y_sep "❱---------------------------------------------------------------------------❰" CACHE INTERNAL "")
 
-###############################################################################
+
+################################################################################
 ## Add package !
 
 function(y_add_library lib_name lib_version lib_url allow_system)
@@ -50,19 +52,19 @@ function(y_add_library lib_name lib_version lib_url allow_system)
         find_package(${lib_name} ${lib_version} QUIET)
     endif()
 
-
-    set(_get_from "System")
     if (NOT ${lib_name}_FOUND)
-        set(_get_from "External")
+        message(STATUS "[y] · External : ${lib_name}")
         FetchContent_Declare(${lib_name} DOWNLOAD_EXTRACT_TIMESTAMP OFF URL ${lib_url})
         FetchContent_MakeAvailable(${lib_name})
+    else()
+        message(STATUS "[y] · System : ${lib_name}")
     endif()
-
-    message(STATUS "y · ${_get_from} : ${lib_name}")
 
     set(_libs_aux "${y_meta_libs} ${lib_name}")
     string(STRIP ${_libs_aux} _libs_clean)
     set(y_meta_libs "${_libs_clean}" CACHE INTERNAL "")
+
+    message(STATUS ${__y_sep})
 
 endfunction()
 
@@ -76,32 +78,31 @@ function(y_add_system lib_name lib_version lib_url)
 endfunction()
 
 
-###############################################################################
+################################################################################
 ## Link to project !
 
 function(y_link_libraries proj_name)
 
     string(REPLACE " " ";" _deps "${y_meta_libs}")
-    # message(STATUS "y ··· Linking : ${proj_name} |${_deps}|")
-    message(STATUS "y ··· Linking -> ${y_meta_libs}")
+    # message(DEBUG "[y]·· Linking -> ${y_meta_libs}")
     target_link_libraries(${proj_name} ${_deps})
 
-endfunction()
+    endfunction()
 
 
-###############################################################################
+################################################################################
 ## Set output dir !
 
 function(y_set_output_dir proj_name dir_name)
 
     if (NOT MSVC)
-        set(_build_type_dir ${CMAKE_BUILD_TYPE})
+    set(_build_type_dir ${CMAKE_BUILD_TYPE})
     else()
         set(_build_type_dir "")
     endif()
 
-    # set(_output_dir "${CMAKE_BINARY_DIR}/../${dir_name}/${_build_type_dir}")
     set(_output_dir "${CMAKE_BINARY_DIR}/../${dir_name}")
+    # message(DEBUG "[y]·· OutputDir -> ${_output_dir}")
 
     set_target_properties(${proj_name} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY "${_output_dir}")
     set_target_properties(${proj_name} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${_output_dir}")
@@ -110,7 +111,7 @@ function(y_set_output_dir proj_name dir_name)
 endfunction()
 
 
-###############################################################################
+################################################################################
 ## Glob !
 
 function(y_glob root_dir out_sources out_headers)
@@ -121,11 +122,24 @@ function(y_glob root_dir out_sources out_headers)
     file(GLOB _headers "${root_dir}/*.hpp" "${root_dir}/*.hh" "${root_dir}/*.h")
     set(${out_headers} "${_headers}" PARENT_SCOPE)
 
+    endfunction()
+
+
+################################################################################
+## Dir Has CMakeLists.txt !
+
+function (y_has_cmakefile root_dir out_has_cmakefile)
+    file(GLOB _cmake_file "${root_dir}/[Cc][Mm][Aa][Kk][Ee][Ll][Ii][Ss][Tt][Ss].txt")
+
+    if(_cmake_file)
+        set(${out_has_cmakefile} ON PARENT_SCOPE)
+    else()
+        set(${out_has_cmakefile} OFF PARENT_SCOPE)
+    endif()
 endfunction()
 
 
-
-###############################################################################
+################################################################################
 ## Executable !
 
 function(y_add_exe proj_name proj_root_dir)
@@ -135,15 +149,23 @@ function(y_add_exe proj_name proj_root_dir)
 
     y_glob(${proj_root_dir} _sources _headers)
 
-    message(STATUS "y · ${proj_name} -- ${proj_root_dir} -- ${_sources}")
-    # message(STATUS "y · ${proj_name} -- ${_headers}")
+    message(DEBUG "[y] · ${proj_name}")
+    message(DEBUG "  -- ${proj_root_dir}")
+    message(DEBUG "  -- Sources:")
+    foreach(_source IN LISTS _sources)
+        message(DEBUG "  ---- ${_source}")
+    endforeach()
+    message(DEBUG "  -- Headers:")
+    foreach(_header IN LISTS _headers)
+        message(DEBUG "  ---- ${_header}")
+    endforeach()
 
     add_executable(${proj_name} ${_sources} ${_headers})
 
 endfunction()
 
 
-###############################################################################
+################################################################################
 ## Top setup !
 
 macro(y_set_defaults cxx_standard)
@@ -163,7 +185,7 @@ macro(y_set_defaults cxx_standard)
 endmacro()
 
 
-###############################################################################
+################################################################################
 ## Target setup !
 
 function(y_setup_exe_project)
@@ -197,7 +219,7 @@ function(y_setup_exe_project)
 endfunction()
 
 
-###############################################################################
+################################################################################
 ## Projects in curr path !
 
 function(y_auto_projects)
@@ -214,31 +236,36 @@ function(y_auto_projects)
     foreach(_item ${_root_content})
         list(FIND _ignore_list "${_item}" _ignored)
 
-        if((_ignored EQUAL -1) AND (IS_DIRECTORY "${_root_dir}/${_item}"))
-            string(SUBSTRING "${_item}" 0 1 _first)
+        set(_item_dir "${_root_dir}/${_item}")
 
+        set(_valid (_ignored EQUAL -1))
+        set(_is_dir (IS_DIRECTORY "${_item_dir}"))
+        y_has_cmakefile("${_item_dir}" _has_cmakefile)
+
+        if(${_valid} AND ${_is_dir} AND ${_has_cmakefile})
+            string(SUBSTRING "${_item}" 0 1 _first)
             if(NOT (_first STREQUAL "."))
                 list(APPEND _found_dirs "${_item}")
             endif()
-
         endif()
     endforeach()
 
     # Add subdirs
     foreach(_dir ${_found_dirs})
-        message(STATUS "y · Project : ${_dir}")
+        message(STATUS "[y] · Project : ${_dir}")
         add_subdirectory("${_dir}")
+        message(STATUS ${__y_sep})
     endforeach()
 
 endfunction()
 
 
-###############################################################################
+################################################################################
 ## Tests !
 
 function(y_enable_tests)
 
-    message(STATUS "y · Enabling tests")
+    message(STATUS "[y] · Enabling tests")
 
     enable_testing()
 
@@ -254,8 +281,12 @@ function(y_enable_tests)
 
         add_test(NAME "${_name}" COMMAND "${_name}")
 
-        message(STATUS "y · Test : ${_name} from '${_source}'")
+        message(STATUS "[y] · Test : '${_name}' from '${_source}'")
 
     endforeach()
+    message(STATUS ${__y_sep})
 
 endfunction()
+
+
+################################################################################
